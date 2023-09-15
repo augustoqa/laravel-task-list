7b308a35f6e15b0e5bfb3d047f552e37ee77639a
Augusto Quisquinay <augustoacual@gmail.com>
2023-09-15 17:05:35 -0600

    Create Form to save a task

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,1 +1,27 @@
-Im a working create view!
+@extends('layouts.app')
+
+@section('title', 'Create a Task')
+
+@section('content')
+<form action="{{ route('tasks.store') }}" method="POST">
+    @csrf
+    <div>
+        <label for="title">Title</label>
+        <input type="text" name="title" id="title">
+    </div>
+
+    <div>
+        <label for="description">Description</label>
+        <textarea name="description" id="description" rows="5"></textarea>
+    </div>
+
+    <div>
+        <label for="long_description">Long Description</label>
+        <textarea name="long_description" id="long_description" rows="10"></textarea>
+    </div>
+
+    <div>
+        <button type="submit">Add Task</button>
+    </div>
+</form>
+@endsection
