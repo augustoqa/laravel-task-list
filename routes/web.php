183ebf8aa41b067e1495dc8e01b95cbe39f6c761
Augusto Quisquinay <augustoacual@gmail.com>
2023-11-01 17:06:33 -0600

    Refactoring - Use Route Model Binding

--- routes/web.php
+++ routes/web.php
@@ -27,15 +27,15 @@ Route::get('/tasks', function () {
 
 Route::view('/tasks/create', 'create')->name('tasks.create');
 
-Route::get('/tasks/{id}/edit', function ($id) {
+Route::get('/tasks/{task}/edit', function (Task $task) {
     return view('edit', [
-        'task' => Task::findOrFail($id),
+        'task' => $task,
     ]);
 })->name('tasks.edit');
 
-Route::get('/tasks/{id}', function ($id) {
+Route::get('/tasks/{task}', function (Task $task) {
     return view('show', [
-        'task' => Task::findOrFail($id),
+        'task' => $task,
     ]);
 })->name('tasks.show');
 
@@ -57,14 +57,13 @@ Route::post('/tasks', function (Request $request) {
         ->with('success', 'Task created successfully!');
 })->name('tasks.store');
 
-Route::put('/tasks/{id}', function ($id, Request $request) {
+Route::put('/tasks/{task}', function (Task $task, Request $request) {
     $data = $request->validate([
         'title' => 'required|max:255',
         'description' => 'required',
         'long_description' => 'required',
     ]);
 
-    $task = Task::findOrFail($id);
     $task->title = $data['title'];
     $task->description = $data['description'];
     $task->long_description = $data['long_description'];
