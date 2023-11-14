9ecfbe43c2c6a4a175398ba8be94694aec7dafef
Augusto Quisquinay <augustoacual@gmail.com>
2023-11-13 19:20:10 -0600

    Add feature to toggle task status

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -10,6 +10,24 @@
 <p>{{ $task->created_at }}</p>
 <p>{{ $task->updated_at }}</p>
 
+<p>
+    {{ $task->completed ? 'Completed' : 'Not Completed' }}
+</p>
+
+<div>
+    <a href="{{ route('tasks.edit', ['task' => $task]) }}">Edit</a>
+</div>
+
+<div>
+    <form action="{{ route('tasks.toggle', ['task' => $task]) }}" method="post">
+        @csrf
+        @method('PUT')
+        <button>
+            Mark as {{ $task->completed ? 'not completed' : 'completed' }}
+        </button>
+    </form>
+</div>
+
     <div>
         <form action="{{ route('tasks.destroy', ['task' => $task]) }}" method="post">
             @csrf @method('DELETE')
