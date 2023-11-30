0e2792c3cdb9648be2944294d312194971970007
Augusto Quisquinay <augustoacual@gmail.com>
2023-11-29 18:21:19 -0600

    Add styles to task details

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,35 +3,41 @@
 @section('title', $task->title)
 
 @section('content')
-<p>{{ $task->description }}</p>
-
-<p>{{ $task->long_description }}</p>
-
-<p>{{ $task->created_at }}</p>
-<p>{{ $task->updated_at }}</p>
-
-<p>
-    {{ $task->completed ? 'Completed' : 'Not Completed' }}
-</p>
-
-<div>
-    <a href="{{ route('tasks.edit', ['task' => $task]) }}">Edit</a>
+<div class="mb-4">
+    <a href="{{ route('tasks.index') }}" class="link">
+        &larr; Go Back to the task list!
+    </a>
 </div>
 
-<div>
+<p class="mb-4 text-slate-700">{{ $task->description }}</p>
+
+@if ($task->long_description)
+    <p class="mb-4 text-slate-700">{{ $task->long_description }}</p>
+@endif
+
+<p class="mb-4 text-sm text-slate-500">
+    Created {{ $task->created_at->diffForHumans() }} • Updated {{ $task->updated_at->diffForHumans() }}
+</p>
+
+<p class="mb-4">
+    <span @class(['font-medium', 'text-green-500' => $task->completed, 'text-red-500' => !$task->completed])>
+        {{ $task->completed ? 'Completed' : 'Not Completed' }}
+    </span>
+</p>
+
+<div class="flex gap-2">
+    <a href="{{ route('tasks.edit', ['task' => $task]) }}" class="btn">Edit</a>
+
     <form action="{{ route('tasks.toggle', ['task' => $task]) }}" method="post">
         @csrf
         @method('PUT')
-        <button>
+        <button class="btn">
             Mark as {{ $task->completed ? 'not completed' : 'completed' }}
         </button>
     </form>
+    <form action="{{ route('tasks.destroy', ['task' => $task]) }}" method="post">
+        @csrf @method('DELETE')
+        <button class="btn">Delete</button>
+    </form>
 </div>
-
-    <div>
-        <form action="{{ route('tasks.destroy', ['task' => $task]) }}" method="post">
-            @csrf @method('DELETE')
-            <button>Delete</button>
-        </form>
-    </div>
 @endsection
